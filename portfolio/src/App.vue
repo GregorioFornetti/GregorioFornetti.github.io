9103dddf3bcaa7f4047120305ad405cb14b7918a
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2025-12-21 21:24:11 +0100

    cleanup

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark')
 } else {
@@ -20,19 +18,16 @@ function toggleDarkMode() {
 </script>
 
 <template>
-  <div class="dark:bg-black">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-
   <button @click="toggleDarkMode" class="m-4 p-2 bg-gray-200 dark:bg-gray-800 rounded">
     Toggle Dark Mode
   </button>
-  <HelloWorld msg="Vite + Vue" />
+
+  <h1>{{ $t('home.test') }}</h1>
+  <div class="locale-changer">
+    <select v-model="$i18n.locale">
+      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+    </select>
+  </div>
 </template>
 
 <style scoped>
