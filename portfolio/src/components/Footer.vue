8c19bae99259901b7843a44170e342366dd96563
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-01-10 18:12:37 +0100

    Footer project link

--- portfolio/src/components/Footer.vue
+++ portfolio/src/components/Footer.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-
+    const githubLink = '<a class="text-blue-600 dark:text-blue-400 hover:underline" target="_blank" href="https://github.com/GregorioFornetti/GregorioFornetti.github.io">GitHub</a>';
 </script>
 
 <template>
     <footer class="bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700 py-6 mt-12">
         <div class="max-w-7xl mx-auto px-4 text-center text-sm text-gray-500 dark:text-gray-400">
-        &copy; {{ new Date().getFullYear() }} Your Name. All rights reserved.
+            <p v-html="$t('footer.project_text', { githubLink })">
+            </p>
         </div>
     </footer>
 </template>
--- portfolio/src/plugins/i18n.ts
+++ portfolio/src/plugins/i18n.ts
@@ -6,6 +6,9 @@ import home_en from '../content/en/home.json';
 import header_ptBR from '../content/pt-BR/header.json';
 import header_en from '../content/en/header.json';
 
+import footer_ptBR from '../content/pt-BR/footer.json';
+import footer_en from '../content/en/footer.json';
+
 const I18n = createI18n({
   // For use with Vue Composition API
   legacy: false,
@@ -13,11 +16,13 @@ const I18n = createI18n({
   messages: {
     'PT-BR': {
         'home': home_ptBR,
-        'header': header_ptBR
+        'header': header_ptBR,
+        'footer': footer_ptBR
     },
     'EN': {
         'home': home_en,
-        'header': header_en
+        'header': header_en,
+        'footer': footer_en
     }
   }
 });
