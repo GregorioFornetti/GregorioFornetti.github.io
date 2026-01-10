1927999e29b040bb30fe9174427b6c1e1d901c31
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-01-10 21:32:37 +0100

    footer links

--- portfolio/src/components/Footer.vue
+++ portfolio/src/components/Footer.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+    import LattesIcon from './LattesIcon.vue';
     const githubLink = '<a class="text-blue-600 dark:text-blue-400 hover:underline" target="_blank" href="https://github.com/GregorioFornetti/GregorioFornetti.github.io">GitHub</a>';
 </script>
 
@@ -7,6 +8,44 @@
         <div class="max-w-7xl mx-auto px-4 text-center text-sm text-gray-500 dark:text-gray-400">
             <p v-html="$t('footer.project_text', { githubLink })">
             </p>
+            <div class="mt-5">
+                <a class="social-media-btn" href="mailto:gregorio.fornetti@estudante.ufscar.br">
+                    <font-awesome-icon :icon="['fas', 'envelope']" />
+                </a>
+                <a class="social-media-btn" target="_blank" href="https://github.com/GregorioFornetti">
+                    <font-awesome-icon :icon="['fab', 'github']" />
+                </a>
+                <a class="social-media-btn" target="_blank" href="https://www.linkedin.com/in/greg%C3%B3rio-fornetti-azevedo-4a0193201/">
+                    <font-awesome-icon :icon="['fab', 'linkedin']" />
+                </a>
+                <a class="social-media-btn" target="_blank" href="https://scholar.google.com/citations?user=3dG7e1IAAAAJ">
+                    <font-awesome-icon :icon="['fab', 'google-scholar']" />
+                </a>
+                <a class="social-media-btn" target="_blank" href="https://orcid.org/0000-0002-1096-7456">
+                    <font-awesome-icon :icon="['fab', 'orcid']" />
+                </a>
+                <a class="social-media-btn" target="_blank" href="http://lattes.cnpq.br/0270898882568377">
+                    <LattesIcon />
+                </a>
+            </div>
         </div>
     </footer>
 </template>
+
+<style scoped>
+.social-media-btn {
+    @apply text-gray-500 dark:text-gray-400
+           hover:text-blue-600 dark:hover:text-blue-400
+           transition-colors
+           mx-3
+           text-2xl;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.social-media-btn svg {
+  width: 1em;
+  height: 1em;
+}
+</style>
