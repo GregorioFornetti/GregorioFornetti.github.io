d5477b537bf2ce38e0d99fae65232a48ba0bd8d5
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-01-23 22:33:05 +0100

    Add content to the experience page

--- portfolio/src/components/pages/Experience.vue
+++ portfolio/src/components/pages/Experience.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts">
+import Experience from '../Experience.vue';
 </script>
 
 <template>
-    <h1>Experience</h1>
+    <h2>{{  $t('experience.education_title')  }}</h2>
+    <Experience
+        v-for="education in ($tm('experience.education') as any[])"
+        :key="education.title + education.institution"
+        :title="education.title"
+        :institution="education.institution"
+        :location="education.location"
+        :startDate="education.start_date"
+        :endDate="education.end_date"
+        :summary="education.summary"
+        :long_description="education.long_description"
+    />
+
+    <h2 class="mt-12">{{  $t('experience.internships_title')  }}</h2>
+    <Experience
+        v-for="internship in ($tm('experience.internships') as any[])"
+        :key="internship.title + internship.institution"
+        :title="internship.title"
+        :institution="internship.institution"
+        :location="internship.location"
+        :startDate="internship.start_date"
+        :endDate="internship.end_date"
+        :summary="internship.summary"
+        :long_description="internship.long_description"
+    />
 </template>
 
 <style scoped>
