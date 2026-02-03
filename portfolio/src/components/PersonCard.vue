889127a57d570da520fa6b9047f67020b2c5251a
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-02-03 21:23:59 +0100

    people img fallback

--- portfolio/src/components/PersonCard.vue
+++ portfolio/src/components/PersonCard.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   name: string,
   photoPath: string,
-  linkedinUrl: string
+  fallBackPhotoPath: string,
+  url: string
 }>();
+
+const imgSrc = ref(props.photoPath);
+
+function handleImageError() {
+  console.log('Image failed to load, using fallback.');
+  console.log('Fallback path:', props.fallBackPhotoPath);
+  imgSrc.value = props.fallBackPhotoPath;
+}
 
 </script>
 
 <template>
   <div class="w-64 flex flex-col items-center">
-    <a :href="linkedinUrl" target="_blank" rel="noopener noreferrer">
-        <img class="w-48 h-48 rounded-full mb-4" :src="photoPath" :alt="name" />
+    <a :href="url" target="_blank" rel="noopener noreferrer">
+        <img class="w-48 h-48 rounded-full mb-4" :src="imgSrc" :alt="name" @error="handleImageError" />
     </a>
     <span class="font-bold text-2xl mb-2 text-gray-900 text-center dark:text-white">{{ name }}</span>
   </div>
--- portfolio/src/components/pages/People.vue
+++ portfolio/src/components/pages/People.vue
@@ -6,8 +6,9 @@ import PersonCard from '../PersonCard.vue';
 <template>
     <PersonCard
         name="Gregório Fornetti Azevedo"
-        photoPath="/assets/avatar.jpeg"
-        linkedinUrl="https://www.linkedin.com/in/janedoe"
+        photoPath="https://media.licdn.com/dms/image/v2/D4D03AQH67kVUpq45Dg/profile-displayphoto-scale_400_400/B4DZwKHwSeHYAk-/0/1769696332672?e=1771459200&v=beta&t=jtahC0dTUZ6qox6Vkrr42i5y23h-sUhFBhYT3ap5mmw"
+        fallBackPhotoPath="/content/people/andre/avatar.jpg"
+        url="https://www.linkedin.com/in/janedoe"
     />
 </template>
 
