e875c0846a6aad53f6b1f6af49479d199430ff68
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-02-05 21:47:08 +0100

    Add supervisors info

--- portfolio/src/components/pages/People.vue
+++ portfolio/src/components/pages/People.vue
@@ -2,18 +2,33 @@
 import PersonCard from '../PersonCard.vue';
 import peopleJson from '../../loaders/peopleInfo';
 
-console.log(peopleJson);
-
 </script>
 
 <template>
-    <PersonCard
-        v-bind="peopleJson['andre_medeiros']"
-    />
-    <PersonCard
-        v-bind="peopleJson['tiago_almeida']"
-    />
+    <h2 class="people-title">{{  $t('people.msc_supervisors_title')  }}</h2>
+    <div class="people-container">
+        <PersonCard
+            v-bind="peopleJson['tiago_almeida']"
+        />
+        <PersonCard
+            v-bind="peopleJson['renato_moraes']"
+        />
+    </div>
+
+    <h2 class="people-title">{{  $t('people.bsc_supervisors_title')  }}</h2>
+    <div class="people-container">
+        <PersonCard
+            v-bind="peopleJson['tiago_almeida']"
+        />
+    </div>
 </template>
 
 <style scoped>
+.people-title {
+    @apply text-4xl font-bold tracking-tight text-gray-900 dark:text-white mb-5 text-center mt-20 mb-12;
+}
+
+.people-container {
+    @apply flex gap-10 justify-center flex-wrap;
+}
 </style>
