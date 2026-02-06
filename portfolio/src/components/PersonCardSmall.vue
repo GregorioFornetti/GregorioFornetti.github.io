c042045486756d3d81d9cba0574b208c65482ef7
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-02-06 22:04:39 +0100

    Create PersonCardSmall.vue

---
+++ portfolio/src/components/PersonCardSmall.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+  name: string,
+  photoPath: string,
+  fallBackPhotoPath: string,
+  url: string
+}>();
+
+const imgSrc = ref(props.photoPath);
+
+function handleImageError() {
+  console.log('Image failed to load, using fallback.');
+  console.log('Fallback path:', props.fallBackPhotoPath);
+  imgSrc.value = props.fallBackPhotoPath;
+}
+
+</script>
+
+<template>
+  <div class="w-40 flex flex-col items-center">
+    <a :href="url" target="_blank" rel="noopener noreferrer">
+        <img class="w-32 h-32 rounded-full mb-4" :src="imgSrc" :alt="name" @error="handleImageError" />
+    </a>
+    <span class="font-bold text-lg mb-2 text-gray-900 text-center dark:text-white">{{ name }}</span>
+  </div>
+</template>
