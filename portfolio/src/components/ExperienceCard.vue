1918940512d0c5c9a9ceb866a30bc2740fdf3cfc
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-02-15 21:46:46 +0100

    Update ExperienceCard.vue

--- portfolio/src/components/ExperienceCard.vue
+++ portfolio/src/components/ExperienceCard.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { marked } from 'marked'
 import { ref } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   title: string,
   institution: string,
   location: string,
@@ -12,6 +14,9 @@ defineProps<{
 }>();
 
 const showingMore = ref(false);
+
+const summaryMd = computed(() => marked.parse(props.summary));
+const longDescriptionMd = computed(() => marked.parse(props.long_description));
 
 </script>
 
@@ -28,7 +33,7 @@ const showingMore = ref(false);
       <span v-else> - Present</span>
     </p>
     <div v-if="!showingMore">
-      <p class="text-gray-900 dark:text-white">{{ summary }}</p>
+      <p class="text-gray-900 dark:text-white" v-html="summaryMd"></p>
       <button
         class="text-blue-600 dark:text-blue-400 hover:underline"
         @click="showingMore = !showingMore"
@@ -37,7 +42,7 @@ const showingMore = ref(false);
       </button>
     </div>
     <div v-else>
-      <p class="text-gray-900 dark:text-white">{{ long_description }}</p>
+      <p class="text-gray-900 dark:text-white" v-html="longDescriptionMd"></p>
       <button
         class="text-blue-600 dark:text-blue-400 hover:underline"
         @click="showingMore = !showingMore"
