08bd0a7419fac29a1b52a2591a8c93627eb910b5
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-02-25 22:12:39 +0100

    Update main.ts

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -5,8 +5,10 @@ import './index.css'
 import './plugins/marked_config'
 import './plugins/fontawessome'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { router } from './plugins/router'
 
 const app = createApp(App);
 app.use(i18n);
+app.use(router);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
