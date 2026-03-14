9bfaf0e69ec92868e0019bd9747c371e3a64313a
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-03-14 19:16:05 +0100

    add modal opening with query params

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -2,6 +2,11 @@
 import Header from './components/Header.vue';
 import { i18n } from './plugins/i18n';
 import Footer from './components/Footer.vue';
+import { router } from './plugins/router';
+import PaperCard from './components/card/PaperCard.vue';
+import ProjectCard from './components/card/ProjectCard.vue';
+import { ref } from 'vue';
+
 
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark')
@@ -15,6 +20,23 @@ if (localStorage.locale) {
   localStorage.locale = i18n.locale.value
 }
 
+let modalType = ref<string|undefined>(undefined)
+let info = ref<any>(undefined)
+
+router.isReady().then(() => {
+
+
+  modalType.value = router.currentRoute.value.query.modalType as string
+  let modalId = router.currentRoute.value.query.modalId as string
+
+  if (modalType.value === 'paper') {
+    info.value = (i18n.tm('papers') as any[]).find(p => p.slug === modalId)
+  } else if (modalType.value === 'project') {
+    info.value = (i18n.tm('projects') as any[]).find(p => p.slug === modalId)
+  }
+
+  console.log(info.value)
+})
 </script>
 
 <template>
@@ -23,6 +45,33 @@ if (localStorage.locale) {
 
     <div class="pt-20 px-10">
       <RouterView />
+    </div>
+
+    <div class="hidden">
+      <PaperCard 
+        v-if="modalType === 'paper' && info"
+        :id="info.slug"
+        :title="info.title"
+        :imgPath="info.cover"
+        :imgAlt="info.img_alt"
+        :abstractText="info.abstract"
+        :contributionsText="info.contributions_text"
+        :projectUrl="info.url"
+        alreadyOpen
+      />
+
+      <ProjectCard 
+        v-if="modalType === 'project' && info"
+        :id="info.slug"
+        :title="info.title"
+        :imgPath="info.cover"
+        :imgAlt="info.img_alt"
+        :contributionsText="info.contributions_text"
+        :aboutText="info.about_text"
+        :projectUrl="info.url"
+        :projectPeople="info.people"
+        alreadyOpen
+      />
     </div>
 
     <Footer />
