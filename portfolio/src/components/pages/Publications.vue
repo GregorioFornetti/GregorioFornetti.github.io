04ff4bebecb24375e9d2cb24f90cb19a9609f754
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2026-07-08 19:59:20 +0200

    new build v2

--- portfolio/src/components/pages/Publications.vue
+++ portfolio/src/components/pages/Publications.vue
@@ -8,7 +8,7 @@
             <h2 class="text-4xl font-bold text-gray-700 dark:text-gray-300 mb-12">{{ $t('publications.title') }}</h2>
             <div class="flex flex-row flex-wrap justify-center max-w-7xl gap-6">
                 <PaperCard 
-                    v-for="paper in $tm('papers')"
+                    v-for="paper in ($tm('papers') as any[])"
                     :id="paper.slug"
                     :key="paper.title" 
                     :title="paper.title"
